205e68f21ebef38b3f3a30b991814fe5b73c67fc
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-02-19 20:38:53 -0800

    I was wrong the first time, I fixed it

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict";
 let myGrades = [100, 100, 90, 73, 78, 94, 86];
 let myAverage = 0;
-for (let i = 1; i <= myGrades.length; i++) {
+for (let i = 0; i <= myGrades.length - 1; i++) {
     myAverage += myGrades[i];
 }
 myAverage = myAverage / myGrades.length;
 console.log("My grade average is " + myAverage);
+//myGrades and myAverage had not been properly defined, I saw this by using "use strict".//
