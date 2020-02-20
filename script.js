018065b04dc733ce1ce49a9f08c185f22aae8675
yahtzi <yaritzabejarano7220@my.whccd.com>
2020-02-19 20:22:52 -0800

    I finished the first part of the activity

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-myGrades = [100, 100, 90, 73, 78, 94, 86];
-
-myAverage = 0;
-
+"use strict";
+let myGrades = [100, 100, 90, 73, 78, 94, 86];
+let myAverage = 0;
 for (let i = 1; i <= myGrades.length; i++) {
-
-myAverage += myGrades[i];
-
+    myAverage += myGrades[i];
 }
-
 myAverage = myAverage / myGrades.length;
-
 console.log("My grade average is " + myAverage);
